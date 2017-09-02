951a646feecef734ce5ce513d86049bdaecff80b
Ben Kelsey <bfkelsey@gmail.com>
2017-09-02 10:28:44 -0700

    Return domain objects from services instead of full response

--- src/app/shared/service/album.service.ts
+++ src/app/shared/service/album.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { SERVER_URL } from './user.service';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { AlbumResponse, AlbumsResponse } from '../domain/album.domain';
+import { Album, AlbumResponse, Albums, AlbumsResponse } from '../domain/album.domain';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AlbumService {
@@ -17,7 +18,7 @@ export class AlbumService {
     toYear?: string,
     genre?: string,
     musicFolderId?: string
-  }) {
+  }): Observable<Array<Albums>> {
     const defaultOptions = {
       type: 'alphabeticalByName'
     };
@@ -29,14 +30,16 @@ export class AlbumService {
         params = params.set(option, sentOptions[option]);
       }
     }
-    return this.httpClient.get<AlbumsResponse>(`${server}/rest/getAlbumList`, {params: params});
+    return this.httpClient.get<AlbumsResponse>(`${server}/rest/getAlbumList`, {params: params})
+      .map(res => res['subsonic-response'].albumList.album);
   }
 
-  getAlbum(id: string): Observable<AlbumResponse> {
+  getAlbum(id: string): Observable<Album> {
     const server = localStorage.getItem(SERVER_URL);
     const params = new HttpParams()
       .set('id', id);
-    return this.httpClient.get<AlbumResponse>(`${server}/rest/getAlbum`, {params: params});
+    return this.httpClient.get<AlbumResponse>(`${server}/rest/getAlbum`, {params: params})
+      .map(res => res['subsonic-response'].album);
   }
 
   getAlbumImageUrl(id: String) {
