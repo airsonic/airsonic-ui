b06099520e435e69aad48b727992ff599e49bed0
Ben Kelsey <bfkelsey@gmail.com>
2017-09-03 15:57:23 -0700

    Navigate to /login when no server exists
    Signed-off-by: Ben Kelsey <bfkelsey@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './shared/service/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   menuOpen = false;
   collapsed = true;
+
+  constructor(private userService: UserService,
+              private router: Router) {}
+
+  ngOnInit() {
+    if (!this.userService.hasUser()) {
+      this.router.navigateByUrl('/login');
+    }
+  }
 
   openMenu() {
     this.menuOpen = !this.menuOpen;
--- src/app/shared/service/user.service.spec.ts
+++ src/app/shared/service/user.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { User, USER_INFO, UserService } from './user.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UserService', () => {
@@ -13,6 +13,22 @@ describe('UserService', () => {
 
   it('should be created', inject([UserService], (service: UserService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should be false if no user is set', inject([UserService], (service: UserService) => {
+    localStorage.clear();
+    expect(service.hasUser()).toBeFalsy();
+  }));
+
+  it('should be false if no user is set', inject([UserService], (service: UserService) => {
+    const user: User = {
+      name: 'username',
+      salt: 'abc123',
+      token: 'testToken',
+      server: 'localhost:8080'
+    };
+    localStorage.setItem(USER_INFO, JSON.stringify(user));
+    expect(service.hasUser()).toBeTruthy();
   }));
 });
 
