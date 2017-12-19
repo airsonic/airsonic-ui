3f40127cd467b473148aa44dfb8239eb58a503a9
jo <lammler.jonas@riseup.net>
2017-12-19 19:42:42 +0100

    Fix missing username in topbar dropdown menu

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -11,11 +11,22 @@ export class TopBarComponent implements OnInit {
   menuOpen = false;
   collapsed = true;
   query: string;
+  username: string;
 
   constructor(private userService: UserService,
               private router: Router) {}
 
+
+  // Is this clean ? something seems wrong
+  // having duplicate code in app.component.ts and top.component.ts
+  // We need to load username here but redirection to login page
+  // should be done by the app.component ?
   ngOnInit() {
+    if (!this.userService.hasUser()) {
+      this.router.navigateByUrl('/login');
+    } else {
+      this.username = this.userService.getUser().name;
+    }
   }
 
   openMenu() {
