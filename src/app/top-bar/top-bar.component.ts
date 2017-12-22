5b98c9ba7650e5cad9e2c97fb674c3e88766a506
jo <lammler.jonas@riseup.net>
2017-12-22 10:20:38 +0100

    Use @Input to retrieve username

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { UserService } from '../shared/service/user.service';
 import { Router } from '@angular/router';
 
@@ -8,25 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./top-bar.component.scss']
 })
 export class TopBarComponent implements OnInit {
+  @Input() username: string;
   menuOpen = false;
   collapsed = true;
   query: string;
-  username: string;
 
   constructor(private userService: UserService,
               private router: Router) {}
 
-
-  // Is this clean ? something seems wrong
-  // having duplicate code in app.component.ts and top.component.ts
-  // We need to load username here but redirection to login page
-  // should be done by the app.component ?
   ngOnInit() {
-    if (!this.userService.hasUser()) {
-      this.router.navigateByUrl('/login');
-    } else {
-      this.username = this.userService.getUser().name;
-    }
+  }
+
+  logout() {
+    this.userService.logout();
+    this.router.navigateByUrl('/login').then(() => {
+      this.closeMenu();
+    });
   }
 
   openMenu() {
@@ -39,13 +36,6 @@ export class TopBarComponent implements OnInit {
 
   closeMenu() {
     this.menuOpen = false;
-  }
-
-  logout() {
-    this.userService.logout();
-    this.router.navigateByUrl('/login').then(() => {
-      this.closeMenu();
-    });
   }
 
   onSearch(query: string) {
