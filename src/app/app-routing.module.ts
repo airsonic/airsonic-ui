c229e3f63ab4b1ce66a657b2525e99ff59ec85d8
jo <lammler.jonas@riseup.net>
2017-12-26 00:15:25 +0100

    Generate settings component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SettingsComponent } from './settings/settings.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { AlbumComponent } from './album/album.component';
 import { SearchResultComponent } from './search-result/search-result.component';
@@ -9,6 +10,7 @@ import { SearchResultComponent } from './search-result/search-result.component';
   imports: [ RouterModule.forRoot([
     { path: '', component: AlbumsComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'settings', component: SettingsComponent },
     { path: 'album/:id', component: AlbumComponent },
     { path: 'search/:query', component: SearchResultComponent }
   ]) ],
